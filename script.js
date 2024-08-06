b6c8248cf2e71e606ba495ab03647020eddaf338
Anatoly <contact@anafro.ru>
2024-08-06 16:47:44 +1000

    fix: make input empty if storage is empty (instead of `undefined`)

--- script.js
+++ script.js
@@ -2,12 +2,10 @@ document.addEventListener('DOMContentLoaded', () => {
     const input = document.getElementById("snils-input");
 
     chrome.storage.local.get(["code"]).then(({code}) => {
-        input.value = code;
+        input.value = code ?? "";
     });
 
-    console.log("yo")
     input.addEventListener("input", () => {
-        console.log("Nice!")
         const code = input.value;
         const regex = /^\d{3}-\d{3}-\d{3} \d{2}$/;
 
